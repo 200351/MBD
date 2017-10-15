66b3bc6637e206988c7cf90dbc373b5dda722489
BartoszZyc <bartosz.z.93@gmail.com>
2017-10-15 16:53:49 +0200

    Pierwszy algorytm. Struktura projektu. Testy. New test

--- ComparatorTests/Controller/Comparator/Impl/NumberOfLettersTests.cs
+++ ComparatorTests/Controller/Comparator/Impl/NumberOfLettersTests.cs
@@ -33,6 +33,25 @@ namespace Comparator.Tests
         }
 
         [TestMethod()]
+        public void testIsEqualsLenghtRemoveWhiteSpacedAndPunctuationTest()
+        {
+            IComparator comparator = new NumerOfLettersComparator();
+            ComparationInput input = new ComparationInput();
+            input.filename1 = "filename1";
+            input.filename2 = "filename2";
+            input.file1 = "test,te   stt.est";
+            input.file2 = "e   st,test.testt";
+
+            ComparationResult result = comparator.compare(input);
+
+            Assert.AreEqual(1, result.score);
+            Assert.AreEqual(1, result.weigth);
+            Assert.AreEqual(input.filename1, result.filename1);
+            Assert.AreEqual(input.filename2, result.filename2);
+
+        }
+
+        [TestMethod()]
         public void testIsNotEqualsLenghtTest()
         {
             IComparator comparator = new NumerOfLettersComparator();
